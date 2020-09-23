4abcbe6455a698297ac241c7ab9a8a32af70dedf
Josh Archer <jarcher@greenriver.edu>
2020-09-23 12:55:01 -0700

    Adding an example website for selecting elements in a web page through JS.

---
+++ intro_to_js/scripts/code.js
@@ -0,0 +1,13 @@
+//put the paragraph element in a variable
+var para = document.querySelector("p");
+
+//change the text in the paragraph element
+paragraph.innerHTML = "Welcome to my page!";
+
+console.log("Changed the heading for the page");
+
+//make another change...
+var paragraph2 = document.querySelector("#second-paragraph");
+paragraph2.innerHTML = "Hope you like it!!!";
+
+console.log("Changed the content of the page");
---
+++ wa_facts/scripts/facts.js
@@ -0,0 +1,6 @@
+//select and change the first fact
+var fact1 = document.getElementById("fact-1");
+fact1.innerHTML = "The Lewis and Clark expedition entered the state on October 10, 1805.";
+
+var fact2 = document.getElementById("fact-2");
+fact2.innerHTML = "During World War II, Seattle was the point of departure for many soldiers in the Pacific, a number of which were quartered at Golden Gardens Park.";
