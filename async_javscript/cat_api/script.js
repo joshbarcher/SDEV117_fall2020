1becc0866aaa726f3d90c89dbce9a09c60c99881
Josh Archer <jarcher@greenriver.edu>
2020-11-25 11:38:41 -0800

    Added an example of making a call with the fetch api to a remote server.

--- async_javscript/cat_api/script.js
+++ async_javscript/cat_api/script.js
@@ -12,10 +12,22 @@ function loadCat()
         mode: "cors", //<-- cross origin resource sharing (security setting)
         headers: {
             "Context-Type": "application/json",
-            "x-api-key": ""
+            "x-api-key": "cab8736c-d9ae-481e-8e20-4fe581e5cb09"
         }
     };
-    fetch("https://api.thecatapi.com/v1/images/search", securityValues);
+    fetch("https://api.thecatapi.com/v1/images/search", securityValues)
+        .then(function(response) {
+            //we have our response now, get the JSON data from the response
+            return response.json();
+        })
+        .then(function(json) {
+            //do something with the data
+            console.log(json);
+
+            //change the cat image on the page
+            let image = document.getElementById("portrait");
+            image.setAttribute("src", json[0].url);
+        });
 }
 
 
